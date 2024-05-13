d7a241e3a98e7ba1e0497e718b8d9fec040125a1
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-05-13 11:08:35 +0600

    getting foods by email

--- index.js
+++ index.js
@@ -51,11 +51,20 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { _id: 0, title: 1, imdb: 1 },
+        projection: {  foodName: 1, foodImageUrl: 1, foodCategory: 1, price: 1, userName: 1, description: 1, foodOrigin: 1 },
       };
-      const result = await allFoodItemsCollection.findOne(query);
+      const result = await allFoodItemsCollection.findOne(query, options);
       res.send(result);
     })
+
+    // get all food added by user, filter it with email
+    app.get("/allFoodItems/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await allFoodItemsCollection.find(query).toArray();
+      res.send(result);
+    })
+
     // get all food images from all food items collection
     
 
