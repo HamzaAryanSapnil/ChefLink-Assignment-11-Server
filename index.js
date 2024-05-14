bab2c266b8c62ea6d7be41b4f82295e647f8d5cb
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-05-14 19:00:13 +0600

    deployed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://cheflink-d1e5b.web.app"],
     credentials: true,
   })
 );
